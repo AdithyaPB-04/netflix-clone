75815b5db9014f3089f91ace5d51e0bc3a1ee326
Adithya P B <adithya.binu@appsteamtechnologies.com>
2025-07-16 21:41:05 +0530

    initial commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,25 +2,31 @@ import React from 'react'
 import './Navbar.css'
 import logo from './../../assets/logo.png'
 import search_icon from './../../assets/search_icon.svg'
+import bell_icon from './../../assets/bell_icon.svg'
+import profile_icon from './../../assets/profile_img.png'
+import caret_icon from './../../assets/caret_icon.svg'
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="navbar">
-        <div className="navbar-left">
-          <img src={logo} alt="" />
-          <ul>
-            <li>Home</li>
-            <li>TV Shows</li>
-            <li>Movies</li>
-            <li>News & Popular</li>
-            <li>My List</li>
-            <li>Browser by Languages</li>
-          </ul>
-        </div>
-        <div className="navbar-right">
-          <img src={search_icon} alt="" className='icons'></img>
-        </div>
+      <div className="navbar-left">
+        <img src={logo} alt="" />
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>News & Popular</li>
+          <li>My List</li>
+          <li>Browser by Languages</li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <img src={search_icon} alt="" className="icons"></img>
+        <p>Children</p>
+        <img src={bell_icon} alt="" className="icons"></img>
+        <div className="navbar-profile"></div>
+        <img src={profile_icon} alt="" className="profile"></img>
+        <img src={caret_icon} alt="" ></img>
       </div>
     </div>
   );
